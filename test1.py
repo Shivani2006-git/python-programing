6ca0ad70aa66763ca4dc8d80b7e5742807f4a5ae
Shivani2006-git <shivaniselvarajadurai@gmail.com>
2025-04-23 19:09:53 +0530

    modified test1.py

--- test1.py
+++ test1.py
@@ -1,2 +1,10 @@
-print("helloworld")
-  
+import mysql.connector
+
+conn=mysql.connector.connect(
+    host="localhost",
+    username="root2",
+    password="Pass@1234"
+    
+)
+if conn.is_connected():
+    print("connected")
